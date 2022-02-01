ae34d4df0608322b05c1e118389a37d020f9406e
Abhishek Lad <abhisheklad12@gmail.com>
2022-02-01 21:06:58 +1100

    added ragehook and bumped up FS version

--- src/app/components/cart/cart-button.component.ts
+++ src/app/components/cart/cart-button.component.ts
@@ -14,5 +14,10 @@ export class CartButtonComponent implements OnInit {
 
   ngOnInit(): void {
     this.cartService.getItems().subscribe(items => this.cartBadge = items.length);
+    window.addEventListener('fullstory/rageclick', function(e) {
+      if (window.confirm("Seems like you are stressed. Need a mindfulness break?")) {
+        window.open("https://www.youtube.com/watch?v=ZToicYcHIOU", "Thanks for Visiting!");
+      }   
+    });
   }
 }
